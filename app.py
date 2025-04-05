9f737c82181cc89c58681a4995898bc57b18809e
jagjeetsidhu1281 <jagjeet.singhsidhu@kaltura.com>
2025-04-05 13:48:30 +0800

    added new world

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", message="Hello from your demo web app!")
+    return render_template("index.html", message="Hello from your demo world web app!")
 
 if __name__ == "__main__":
     app.run(debug=True)
